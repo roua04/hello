06ce384dd78129686d73e7492ed1051129f52c9a
roua04 <158773877+roua04@users.noreply.github.com>
2024-08-02 09:32:14 +0200

    Ajout d'une animation de fondu et un cmptr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 
 import { Component } from '@angular/core';
 import { HelloComponent } from './hello/hello.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,26 +1,40 @@
 
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { trigger, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-hello',
-  standalone: true, 
+  standalone: true,
   templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.css']
+  styleUrls: ['./hello.component.css'],
+  imports: [CommonModule], 
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [ // Animation pour l'entrée
+        style({ opacity: 0 }), // Départ avec opacité 0
+        animate('1s', style({ opacity: 1 })) // Animation vers opacité 1 en 1 seconde
+      ])
+    ])
+  ]
 })
 export class HelloComponent implements OnInit {
   title!: string;
   description!: string;
   createdAt!: Date;
-  imageUrl: string = 'https://www.plymouthchiropractic.com/wp-content/uploads/2022/11/welcome-poster-spectrum-brush-strokes-white-background-colorful-gradient-brush-design-vector-paper-illustration-r-welcome-125370796.jpg'; // Image URL
+  imageUrl: string = 'https://www.plymouthchiropractic.com/wp-content/uploads/2022/11/welcome-poster-spectrum-brush-strokes-white-background-colorful-gradient-brush-design-vector-paper-illustration-r-welcome-125370796.jpg';
   message: string = '';
+  clickCount: number = 0;
   ngOnInit(): void {
     this.title = 'HELLO';
-    this.description = 'New Project!! ';
+    this.description = 'New Project!!';
     this.createdAt = new Date();
   }
   showMessage(): void {
-    this.message = 'Welcome to our new project';
+    this.clickCount++;
+    if (this.clickCount < 3) {
+      this.message = 'Welcome to our new project';
+    } else {
+      this.message = ''; // Cache le message quand le nombre de clics est supérieur à 3
+    }
   }
 }
-
-
-
